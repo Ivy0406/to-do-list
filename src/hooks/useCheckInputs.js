93ca05e468f32729f84f885732b78e1a96074f45
IvyYang <ivy990030@icloud.com>
2026-01-25 14:24:13 +0800

    fix: resolve validate logic

--- src/hooks/useCheckInputs.js
+++ src/hooks/useCheckInputs.js
@@ -1,66 +1,69 @@
 import { useState } from "react";
 
 export const useCheckInputs = () => {
-    const [errors, setErrors] = useState({});
-    const validate = (values) => {
-        const newErrors = {};
-        let isValid = true;
+  const [errors, setErrors] = useState({});
+  const validate = (values) => {
+    const newErrors = {};
+    let isValid = true;
 
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        const isEmailEmpty = !values.email.trim();
-        const isEmailInvalid = !emailRegex.test(values.email);
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const isEmailEmpty = !values.email.trim();
+    const isEmailInvalid = !emailRegex.test(values.email);
 
-        const isNicknameEmpty = !values.nickname.trim();
+    const isPasswordEmpty = !values.password.trim();
+    const isPasswordTooShort = values.password.trim().length < 6;
 
-        const isPasswordEmpty = !values.password.trim();
-        const isPasswordTooShort = values.password.trim().length < 6;
+    const isConfirmPasswordEmpty = !values.confirmPassword?.trim();
+    const isPasswordMismatch = values.confirmPassword !== values.password;
 
-        const isConfirmPasswordEmpty = !values.confirmPassword.trim();
-        const isPasswordMismatch = values.confirmPassword !== values.password;
-        
-        if (isEmailEmpty) {
-            newErrors.email = "此欄位不可為空";
-            isValid = false;
-        } else if (isEmailInvalid) {
-            newErrors.email = "Email 格式錯誤";
-            isValid = false;
-        }   else {
-            newErrors.email = "";
-        }
-
-        if (isNicknameEmpty) {
-            newErrors.nickname = "此欄位不可為空";
-            isValid = false;
-        } else {
-            newErrors.nickname = "";
-        }
-
-        if (isPasswordEmpty) {
-            newErrors.password = "此欄位不可為空";
-            isValid = false;
-        } else if (isPasswordTooShort) {
-            newErrors.password = "密碼至少 6 碼";
-            isValid = false;
-        } else {
-            newErrors.password = "";
-        }
-
-        if (isConfirmPasswordEmpty) {
-            newErrors.confirmPassword = "此欄位不可為空";
-            isValid = false;
-        } else if (isPasswordMismatch) {
-            newErrors.confirmPassword = "兩次密碼輸入不一致";
-            isValid = false;
-        } else {
-            newErrors.confirmPassword = "";
-        }
-
-        setErrors(newErrors);
-        return isValid;
+    if (isEmailEmpty) {
+      newErrors.email = "此欄位不可為空";
+      isValid = false;
+    } else if (isEmailInvalid) {
+      newErrors.email = "Email 格式錯誤";
+      isValid = false;
+    } else {
+      newErrors.email = "";
     }
-    const clearErrors = (fieldId) => {
-        setErrors((prev) => ({ ...prev, [fieldId]: "" }));
-    };
 
-    return { errors, validate, clearErrors}
-}
+    if ("nickname" in values) {
+      const isNicknameEmpty = !values.nickname.trim();
+      if (isNicknameEmpty) {
+        newErrors.nickname = "此欄位不可為空";
+        isValid = false;
+      } else {
+        newErrors.nickname = "";
+      }
+    }
+
+    if (isPasswordEmpty) {
+      newErrors.password = "此欄位不可為空";
+      isValid = false;
+    } else if (isPasswordTooShort) {
+      newErrors.password = "密碼至少 6 碼";
+      isValid = false;
+    } else {
+      newErrors.password = "";
+    }
+
+    if ("confirmPassword" in values) {
+      if (isConfirmPasswordEmpty) {
+        newErrors.confirmPassword = "此欄位不可為空";
+        isValid = false;
+      } else if (isPasswordMismatch) {
+        newErrors.confirmPassword = "兩次密碼輸入不一致";
+        isValid = false;
+      } else {
+        newErrors.confirmPassword = "";
+      }
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+  const clearErrors = (fieldId) => {
+    setErrors((prev) => ({ ...prev, [fieldId]: "" }));
+  };
+
+  return { errors, validate, clearErrors };
+};
