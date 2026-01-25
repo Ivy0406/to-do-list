df70cda541c74436b63d700a0bd013bdb22f2d72
IvyYang <ivy990030@icloud.com>
2026-01-25 23:43:10 +0800

    update: adjust empty visual

--- src/components/todo-list/Empty.jsx
+++ src/components/todo-list/Empty.jsx
@@ -2,7 +2,7 @@ import KEY_VISUAL_EMPTY from "../../images/key-visual-empty.svg";
 
 const Empty = () => {
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col items-center gap-4 mt-11">
       <p className="text-text-main font-normal">目前尚無待辦事項</p>
       <div><img src={KEY_VISUAL_EMPTY} alt="無待辦事項" /></div>
     </div>
