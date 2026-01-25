702256d4c7a426917e73e6f270fbb6e5054e795f
IvyYang <ivy990030@icloud.com>
2026-01-25 22:41:54 +0800

    fix: resolve imgs path error

--- src/components/todo-list/Empty.jsx
+++ src/components/todo-list/Empty.jsx
@@ -1,8 +1,10 @@
+import KEY_VISUAL_EMPTY from "../../images/key-visual-empty.svg";
+
 const Empty = () => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center gap-4">
       <p className="text-text-main font-normal">目前尚無待辦事項</p>
-      <div><img src="/src/images/key-visual-empty.svg" alt="無待辦事項" /></div>
+      <div><img src={KEY_VISUAL_EMPTY} alt="無待辦事項" /></div>
     </div>
   );
 };
